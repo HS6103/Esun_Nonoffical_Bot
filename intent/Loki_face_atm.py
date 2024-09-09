7e4a93ce64155a8db94422300fa7f079d4f3c80e
HS6103 <hsuchen1022@gmail.com>
2024-09-09 14:55:27 +0800

    changed reply json filepath format in all intent py

--- intent/Loki_face_atm.py
+++ intent/Loki_face_atm.py
@@ -32,9 +32,9 @@ except Exception as e:
 responseDICT = {}
 if CHATBOT_MODE:
     try:
-        responseDICT = json.load(open(os.path.join(os.path.dirname(os.path.dirname(__file__)), "reply\\reply_face_atm.json"), encoding="utf-8"))
+        responseDICT = json.load(open(os.path.join(os.path.dirname(os.path.dirname(__file__)), os.path.join("reply", "reply_face_atm.json")), encoding="utf-8"))
     except Exception as e:
-        responseDICT = json.load(open(os.path.join(os.path.dirname(os.path.dirname(__file__)), "reply/reply_face_atm.json"), encoding="utf-8"))
+        print("[ERROR] responseDICT => {}".format(str(e)))
 
 # 將符合句型的參數列表印出。這是 debug 或是開發用的。
 def debugInfo(inputSTR, utterance):
